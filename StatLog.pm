bd334e3eff81dfa83182f657ec71489f5a29f378
Steven J. DeRose <sderose@docugami.com>
2021-09-16 14:44:15 -0400

    Cleanup.

--- StatLog.pm
+++ StatLog.pm
@@ -1,17 +1,7 @@
 #!/usr/bin/perl -w
 #
 # StatLog.pm: Keep stats on various events: counts, min, max, etc.
-#
 # 2010-02-25: Written by Steven J. DeRose.
-# 2013-06-14: Split out of checkWordLists to separate package.
-# 2013-01-04: Clean, finish, integrate into 'vocab'. Add min/max/first/last,
-#     and knowledge of types.
-# 2014-02-05: Change set() to hit(), since it doesn't really "set" values.
-#     Add 'events' counter, and don't use 'undef' to mean "haven't seen".
-# 2014-04-10: Add intWidth.
-# 2015-02-20: Speed it up a lot. Ditch hit(). types, event-counting.
-#
-# To do:
 #
 use strict;
 #use utf8;
@@ -19,10 +9,204 @@ use strict;
 
 use sjdUtils;
 
-my $version = "2015-02-20";
+our %metadata = (
+    'title'        => "StatLog",
+    'description'  => "Keep stats on various events: counts, min, max, etc.",
+    'rightsHolder' => "Steven J. DeRose",
+    'creator'      => "http://viaf.org/viaf/50334488",
+    'type'         => "http://purl.org/dc/dcmitype/Software",
+    'language'     => "Perl 5.18",
+    'created'      => "2010-02-2",
+    'modified'     => "2021-09-16",
+    'publisher'    => "http://github.com/sderose",
+    'license'      => "https://creativecommons.org/licenses/by-sa/3.0/"
+);
+our $VERSION_DATE = $metadata{'modified'};
 
 
-###############################################################################
+=pod
+
+=head1 Usage
+
+NOT MAINTAINED
+
+Maintains a list of named statistics, and can generate a report
+of the totals.
+
+In addition to simple counting, there are calls to 
+to set (or reset) a stat, to set only if the new value is greater than
+(or less than) the prior value, to keep only the first or last value
+for the stat, etc.
+
+There are also I<append>() and I<setKey>() methods that let you 
+add new data to an array or hash. Using those calls forces the statistic
+to be of that type, if it's not already (losing any prior data).
+
+You can also merge multiple StatLog instances, for example to keep grand totals
+and per-file totals. However, if you use min/max/first/last
+calls such as just mentioned,
+merging won't maintain their semantics
+(because StatLog doesn't know a "type" per se).
+
+=head2 Example
+
+    use StatLog;
+    my $sl = new StatLog();
+    $sl->create("tooLong", "Records that were too long");
+    $sl->create("syntax",  "Syntax errors");
+    $sl->create("maxrec",  "Length of longest record");
+    ...
+    $sl->max("maxrec", length($buf));
+    if (length($buf) > $LIMIT) {
+        $sl->count("tooLong");
+    }
+    ...
+    $sl->report();
+
+
+=head1 Methods
+
+=over
+
+=item * B<new StatLog>I<()>
+
+Constructor.
+
+=item * B<createMultiple>I<(hashRef)>
+
+Define numerous new statistics. 
+This is merely shorthand for calling I<create>() on each name=>label pair,
+in alphabetical order.
+Returns the number of statistics created.
+
+=item * B<create>I<(name, label)>
+
+Define a new statistic to be kept.
+The label will be used when generating reports (see I<report>(), below).
+The new statistic is initialized to I<undef>.
+
+=item * B<delete>I<(name)>
+
+Entirely delete the named statistic.
+
+=item * B<resetAll>I<()>
+
+Call I<reset()> for each known statistic.
+
+=item * B<reset>I<(name)>
+
+Set the named statistic to C<undef> for scalars, or to [] or {}
+if the value is already of that type. Methods that modify statistic
+values, all know what to do if the value to be modified is C<undef>.
+
+=item * B<get>I<(name)>
+
+Return the current value of the named statistic.
+
+=item * B<set>I<(name. value)>
+
+Set the value of the named statistic directly.
+
+=item * B<count>I<(name, value?)>
+
+Increment the value of the named statistic by I<value> (default: 1).
+
+=item * B<max>I<(name, value)>
+
+Set the named statistic to the greater of I<value> or its current value.
+
+=item * B<min>I<(name, value)>
+
+Set the named statistic to the lesser of I<value> or its current value.
+
+=item * B<first>I<(name, value)>
+
+Set the named statistic to I<value> the first time, and ignore further requests.
+
+=item * B<last>I<(name, value)>
+
+Set the named statistic to I<value> regardless of any prior value, such that the
+last value set is the one that is kept.
+
+=item * B<append>I<(name, value)> or B<push>I<(name, value)>
+
+Push the value onto an array for the named statistic.
+If the value is not already an array, it is replaced by a reference
+to a new array containing only I<value>.
+
+=item * B<setKey>I<(name, key, value)>
+
+Add the I<key> => I<value> pair to a hash for the named statistic.
+If the value is not already a hash, it is replaced by a reference
+to a new hash containing only I<key> => I<value>.
+
+=item * B<report>I<(where)>
+
+Generate a report of all the statistics and their values.
+I<where> defaults to STDOUT.
+Statistics are reported in the order they were created.
+
+=item * B<reportColorLine>I<(label, n, pct, colorName)>
+
+Internal utility used by I<report>.
+
+=item * B<copy>I<()>
+
+Return a new StatLog instance that is a copy of the current one.
+
+=item * B<merge>I<(other)>
+
+Goes through all the statistics in the StatLog instance I<other>, and
+combines them into the instance being invoked. 
+Arrays are concatenated. Hashes are merged.
+Other values are just added, because the package doesn't know
+whether you've used special things like max/min/first/last.
+
+=back
+
+
+=head1 Known bugs and limitations
+
+I<merge>() won't work right in relation to first/last/min/max.
+
+
+=head1 Related commands and files
+
+C<vocab> -- Vocabulary counter, first package to integrate this.
+
+C<sjdUtils.py> -- The Python version of my utilities provides an optional
+C<stat="name"> parameter, that will increment a statistic when issuing
+any error or verbose message. It also has a few other related calls.
+Thus, the StatLog.pm package is not needed in Python.
+
+
+=head1 History
+
+  2010-02-25: Written by Steven J. DeRose.
+  2013-06-14: Split out of checkWordLists to separate package.
+  2013-01-04: Clean, finish, integrate into 'vocab'. Add min/max/first/last,
+and knowledge of types.
+  2014-02-05: Change set() to hit(), since it doesn't really "set" values.
+Add 'events' counter, and don't use 'undef' to mean "haven't seen".
+  2014-04-10: Add intWidth.
+  2015-02-20: Speed it up a lot. Ditch hit(). types, event-counting.
+  2021-09-16: Cleanup.
+  
+
+=head1 Rights
+
+Copyright 2010-02-25 by Steven J. DeRose. This work is licensed under a
+Creative Commons Attribution-Share Alike 3.0 Unported License.
+For further information on this license, see
+L<https://creativecommons.org/licenses/by-sa/3.0>.
+
+For the most recent version, see L<http://www.derose.net/steve/utilities> or
+L<https://github.com/sderose>.
+
+
+=cut
+
+
 ###############################################################################
 # Maintain counts of various statistics by name.
 # New ones need to be explicitly added.
@@ -295,187 +479,3 @@ sub merge {
         }
     }
 } # merge
-
-
-# End package StatLog
-
-
-
-###############################################################################
-###############################################################################
-###############################################################################
-#
-
-=pod
-
-=head1 Usage
-
-Maintains a list of named statistics, and can generate a report
-of the totals.
-
-In addition to simple counting, there are calls to 
-to set (or reset) a stat, to set only if the new value is greater than
-(or less than) the prior value, to keep only the first or last value
-for the stat, etc.
-
-There are also I<append>() and I<setKey>() methods that let you 
-add new data to an array or hash. Using those calls forces the statistic
-to be of that type, if it's not already (losing any prior data).
-
-You can also merge multiple StatLog instances, for example to keep grand totals
-and per-file totals. However, if you use min/max/first/last
-calls such as just mentioned,
-merging won't maintain their semantics
-(because StatLog doesn't know a "type" per se).
-
-=head2 Example
-
-    use StatLog;
-    my $sl = new StatLog();
-    $sl->create("tooLong", "Records that were too long");
-    $sl->create("syntax",  "Syntax errors");
-    $sl->create("maxrec",  "Length of longest record");
-    ...
-    $sl->max("maxrec", length($buf));
-    if (length($buf) > $LIMIT) {
-        $sl->count("tooLong");
-    }
-    ...
-    $sl->report();
-
-
-=for nobody ###################################################################
-
-=head1 Methods
-
-=over
-
-=item * B<new StatLog>I<()>
-
-Constructor.
-
-=item * B<createMultiple>I<(hashRef)>
-
-Define numerous new statistics. 
-This is merely shorthand for calling I<create>() on each name=>label pair,
-in alphabetical order.
-Returns the number of statistics created.
-
-=item * B<create>I<(name, label)>
-
-Define a new statistic to be kept.
-The label will be used when generating reports (see I<report>(), below).
-The new statistic is initialized to I<undef>.
-
-=item * B<delete>I<(name)>
-
-Entirely delete the named statistic.
-
-=item * B<resetAll>I<()>
-
-Call I<reset()> for each known statistic.
-
-=item * B<reset>I<(name)>
-
-Set the named statistic to C<undef> for scalars, or to [] or {}
-if the value is already of that type. Methods that modify statistic
-values, all know what to do if the value to be modified is C<undef>.
-
-=item * B<get>I<(name)>
-
-Return the current value of the named statistic.
-
-=item * B<set>I<(name. value)>
-
-Set the value of the named statistic directly.
-
-=item * B<count>I<(name, value?)>
-
-Increment the value of the named statistic by I<value> (default: 1).
-
-=item * B<max>I<(name, value)>
-
-Set the named statistic to the greater of I<value> or its current value.
-
-=item * B<min>I<(name, value)>
-
-Set the named statistic to the lesser of I<value> or its current value.
-
-=item * B<first>I<(name, value)>
-
-Set the named statistic to I<value> the first time, and ignore further requests.
-
-=item * B<last>I<(name, value)>
-
-Set the named statistic to I<value> regardless of any prior value, such that the
-last value set is the one that is kept.
-
-=item * B<append>I<(name, value)> or B<push>I<(name, value)>
-
-Push the value onto an array for the named statistic.
-If the value is not already an array, it is replaced by a reference
-to a new array containing only I<value>.
-
-=item * B<setKey>I<(name, key, value)>
-
-Add the I<key> => I<value> pair to a hash for the named statistic.
-If the value is not already a hash, it is replaced by a reference
-to a new hash containing only I<key> => I<value>.
-
-=item * B<report>I<(where)>
-
-Generate a report of all the statistics and their values.
-I<where> defaults to STDOUT.
-Statistics are reported in the order they were created.
-
-=item * B<reportColorLine>I<(label, n, pct, colorName)>
-
-Internal utility used by I<report>.
-
-=item * B<copy>I<()>
-
-Return a new StatLog instance that is a copy of the current one.
-
-=item * B<merge>I<(other)>
-
-Goes through all the statistics in the StatLog instance I<other>, and
-combines them into the instance being invoked. 
-Arrays are concatenated. Hashes are merged.
-Other values are just added, because the package doesn't know
-whether you've used special things like max/min/first/last.
-
-=back
-
-
-
-=for nobody ###################################################################
-
-=head1 Known bugs and limitations
-
-I<merge>() won't work right in relation to first/last/min/max.
-
-
-=for nobody ###################################################################
-
-=head1 Related commands and files
-
-C<vocab> -- Vocabulary counter, first package to integrate this.
-
-C<sjdUtils.py> -- The Python version of my utilities provides an optional
-C<stat="name"> parameter, that will increment a statistic when issuing
-any error or verbose message. It also has a few other related calls.
-Thus, the StatLog.pm package is not needed in Python.
-
-
-
-=for nobody ###################################################################
-
-=head1 Ownership
-
-This work by Steven J. DeRose is licensed under a Creative Commons
-Attribution-Share Alike 3.0 Unported License. For further information on
-this license, see L<http://creativecommons.org/licenses/by-sa/3.0/>.
-
-For the most recent version, see L<http://www.derose.net/steve/utilities/>.
-
-=cut
